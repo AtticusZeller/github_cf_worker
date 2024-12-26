8080bbb652fcb4358bea4dbf3192e1b0a1a280e9
A.J.Zeller <atticus.zeller@pm.me>
2024-12-26 17:44:44 +0800

    no cache

--- _worker.js
+++ _worker.js
@@ -3,22 +3,23 @@ let reverseDomainMaps = {};
 const index = {
   async fetch(request, env, _ctx) {
     const needCancel = await needCancelRequest(request);
-    if (needCancel)
-      return new Response("", { status: 204 });
+    if (needCancel) return new Response("", { status: 204 });
     const url = new URL(request.url);
     const { domain, subdomain } = getDomainAndSubdomain(request);
     if (url.pathname === "/robots.txt")
       return new Response("User-agent: *\nDisallow: /", { status: 200 });
     domainMaps = {
-      [`release.${domain}`]: "github.com",  // for github release
+      [`release.${domain}`]: "github.com", // for github release
       [`assets.${domain}`]: "github.githubassets.com",
       [`raw.${domain}`]: "raw.githubusercontent.com",
       [`download.${domain}`]: "codeload.github.com",
       [`objects.${domain}`]: "objects.githubusercontent.com",
       [`media.${domain}`]: "media.githubusercontent.com",
-      [`gist.${domain}`]: "gist.github.com"
+      [`gist.${domain}`]: "gist.github.com",
     };
-    reverseDomainMaps = Object.fromEntries(Object.entries(domainMaps).map((arr) => arr.reverse()));
+    reverseDomainMaps = Object.fromEntries(
+      Object.entries(domainMaps).map((arr) => arr.reverse())
+    );
     if (url.host in domainMaps) {
       url.host = domainMaps[url.host];
       if (url.port !== "80" && url.port !== "443")
@@ -26,11 +27,17 @@ const index = {
       const newRequest = getNewRequest(url, request);
       return proxy(url, newRequest);
     }
-    return new Response(`Unsupported domain ${subdomain ? `${subdomain}.` : ""}${domain}`, {
-      status: 200,
-      headers: { "content-type": "text/plain;charset=utf-8", "git-hash": env.GIT_HASH }
-    });
-  }
+    return new Response(
+      `Unsupported domain ${subdomain ? `${subdomain}.` : ""}${domain}`,
+      {
+        status: 200,
+        headers: {
+          "content-type": "text/plain;charset=utf-8",
+          "git-hash": env.GIT_HASH,
+        },
+      }
+    );
+  },
 };
 function getDomainAndSubdomain(request) {
   const url = new URL(request.url);
@@ -42,7 +49,9 @@ function getDomainAndSubdomain(request) {
     domain = hostArr.slice(1).join(".");
   } else if (hostArr.length === 2) {
     subdomain = hostArr[1].match(/^localhost(:\d+)?$/) ? hostArr[0] : "";
-    domain = hostArr[1].match(/^localhost(:\d+)?$/) ? hostArr[1] : hostArr.join(".");
+    domain = hostArr[1].match(/^localhost(:\d+)?$/)
+      ? hostArr[1]
+      : hostArr.join(".");
   } else {
     domain = hostArr.join(".");
   }
@@ -50,16 +59,23 @@ function getDomainAndSubdomain(request) {
 }
 async function needCancelRequest(request, matches = []) {
   const url = new URL(request.url);
-  matches = matches.length ? matches : [
-    "/favicon.",
-    "/sw.js"
-  ];
+  matches = matches.length ? matches : ["/favicon.", "/sw.js"];
   return matches.some((match) => url.pathname.includes(match));
 }
 function getNewRequest(url, request) {
   const headers = new Headers(request.headers);
   headers.set("reason", "mirror of China");
-  const newRequestInit = { redirect: "manual", headers };
+  // 添加禁用缓存的头部
+  headers.set("Cache-Control", "no-cache, no-store, must-revalidate");
+  headers.set("Pragma", "no-cache");
+  headers.set("Expires", "0");
+
+  const newRequestInit = {
+    redirect: "manual",
+    headers,
+    // 确保不使用缓存
+    cache: "no-store",
+  };
   return new Request(url.toString(), new Request(request, newRequestInit));
 }
 async function proxy(url, request, env) {
@@ -68,6 +84,9 @@ async function proxy(url, request, env) {
     const headers = res.headers;
     const newHeaders = new Headers(headers);
     const status = res.status;
+    newHeaders.set("Cache-Control", "no-cache, no-store, must-revalidate");
+    newHeaders.set("Pragma", "no-cache");
+    newHeaders.set("Expires", "0");
     if (newHeaders.has("location")) {
       const loc = newHeaders.get("location");
       if (loc) {
@@ -89,12 +108,19 @@ async function proxy(url, request, env) {
     newHeaders.delete("clear-site-data");
     if (res.headers.get("content-type")?.indexOf("text/html") !== -1) {
       const body = await res.text();
-      const regAll = new RegExp(Object.keys(reverseDomainMaps).map((r) => `(https?://${r})`).join("|"), "g");
-      const newBody = body.replace(regAll, (match) => {
-        return match.replace(/^(https?:\/\/)(.*?)$/g, (m, p1, p2) => {
-          return reverseDomainMaps[p2] ? `${p1}${reverseDomainMaps[p2]}` : m;
-        });
-      }).replace(/integrity=\".*?\"/g, "");
+      const regAll = new RegExp(
+        Object.keys(reverseDomainMaps)
+          .map((r) => `(https?://${r})`)
+          .join("|"),
+        "g"
+      );
+      const newBody = body
+        .replace(regAll, (match) => {
+          return match.replace(/^(https?:\/\/)(.*?)$/g, (m, p1, p2) => {
+            return reverseDomainMaps[p2] ? `${p1}${reverseDomainMaps[p2]}` : m;
+          });
+        })
+        .replace(/integrity=\".*?\"/g, "");
       return new Response(newBody, { status, headers: newHeaders });
     }
     return new Response(res.body, { status, headers: newHeaders });
